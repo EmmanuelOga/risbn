05612d4176a0f7bc77b64ae2de7c0f912198944c
Emmanuel Oga <EmmanuelOga@gmail.com>
2010-02-24 09:52:31 -0300

    initial commit

---
+++ lib/risbn.rb
@@ -0,0 +1,64 @@
+class RISBN
+  attr_accessor :isbn
+
+  # Gross, but good to catch anything even remotely similar to a ISBN.
+  # Later, we can validate to see if it really is a ISBN.
+  ISBN_REGEXP = /([0-9\-_X]+)/i
+
+  # returns all the isbn numbers found on the line.
+  def self.parse(line)
+    line.split(ISBN_REGEXP).map { |n| new(n) }.select(&:valid?)
+  end
+
+  # Parses the string returning the first valid isbn number found or an empty isbn.
+  def self.parse_first(line)
+    line.split(ISBN_REGEXP).map { |c| n = new(c); return n if n.valid? }; new
+  end
+
+  # Provide a string with the isbn. Any non digit or X character will be removed.
+  def initialize(code = "")
+    @isbn = code.to_s.upcase.gsub(/[^0-9X]/, "")
+  end
+
+  def valid?
+    case isbn.length
+    when 10 then valid_isbn10?
+    when 13 then valid_isbn13?
+    else
+      false
+    end
+  end
+
+  def to_s
+    isbn
+  end
+
+  def checksum
+    last = isbn[-1..-1]
+    (last == 'X') ? 10 : last.to_i
+  end
+
+  def digits
+    isbn[0..-2].split("")
+  end
+
+  # adapted from http://github.com/zapnap/isbn_validation/blob/master/lib/isbn_validation.rb#L48
+  def valid_isbn10?
+    sum = 0
+    digits.each_with_index do |value, index|
+      sum += (index + 1) * value.to_i
+    end
+    (sum % 11) == checksum
+  end
+
+  def valid_isbn13?
+    sum = 0
+    digits.each_with_index do |value, index|
+      multiplier = (index % 2 == 0) ? 1 : 3
+      sum += multiplier * value.to_i
+    end
+    result = 10 - (sum % 10)
+    result = 0 if result == 10
+    result == checksum
+  end
+end
--- spec/risbn_spec.rb
+++ spec/risbn_spec.rb
@@ -1,7 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Risbn" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe RISBN do
+  it "parses isbn numbers from a line of text" do
+    RISBN.parse_first("ISBN-10: 0393317730").to_s.should == "0393317730"
+    RISBN.parse_first("ISBN-13: 978-0393317732").to_s.should == "9780393317732"
+  end
+
+  it "distinguishes valid and invalid isbns" do
+    RISBN.parse_first("ISBN-10: 0393317730").should be_valid
+    RISBN.parse_first("ISBN-13: 978-0393317732").should be_valid
+
+    RISBN.parse_first("ISBN-10: 6663317730").should_not be_valid
+    RISBN.parse_first("ISBN-13: 666-0393317732").should_not be_valid
   end
 end
